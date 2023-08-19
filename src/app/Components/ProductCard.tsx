4086987723ee804c972c128b822866d145f79d20
Md Dulal Hossain <dulalhossain34220@gmail.com>
2023-08-19 18:20:15 +0600

    first commit

--- src/app/Components/ProductCard.tsx
+++ src/app/Components/ProductCard.tsx
@@ -41,7 +41,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       {products?.map((product: any, i: number) => (
         <div key={i} className="relative flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
           <div className="relative mx-4 mt-4 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
-            <Link href={`/products/${product?.id}`}> 
+            <Link href={`/products/${product?.id}`}>
               <a className=''>
                 <Image
                   src={product?.image}
@@ -58,13 +58,18 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
             </Link>
           </div>
           <div className="p-6">
-            <div className="mb-2 flex items-center justify-between">
-              <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
-                {product.title}
-              </p>
-              <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
-                ${product.price.toFixed(2)}
-              </p>
+            <div className="mb-2 flex items-center justify-between h-20">
+              <Link href={`/products/${product?.id}`}>
+                <a>
+                  <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+                    {product.title}
+                    <span className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+                      ${product.price.toFixed(2)}
+                    </span>
+                  </p>
+                </a>
+              </Link>
+
             </div>
             <p className="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
               {product?.description.substring(0, 80)}...
